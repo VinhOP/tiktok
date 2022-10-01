78be13fe04148dec748a6590432efd484599c531
VinhOP <lyquocvinh03@gmail.com>
2022-10-01 17:49:07 +0700

    create Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import Home from '../pages/Home';
+import Following from '../pages/Following';
+import Upload from '../pages/Upload';
+import Search from '../pages/Search';
+import { DefaultLayout, HeaderOnly } from '../components/Layout';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+        layout: DefaultLayout,
+    },
+    {
+        path: '/following',
+        component: Following,
+        layout: DefaultLayout,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
